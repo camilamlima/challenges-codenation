a38b806e6cd13f86dc1e3f914a19bea874d64e02
camilamlima <camilam.oliveiralima@gmail.com>
2019-05-08 12:05:09 +0000

    Adicionado teste da semana 3

--- python-3/main.py
+++ python-3/main.py
@@ -1,21 +1,49 @@
+import re
 import numpy as np
-import datetime
+from datetime import datetime, timedelta
+
 
 class ManagerDates:
     def date_is_valid(self, date):
-        return False
+        try:
+            day, month, year = date.split("/")
+            datetime(int(year), int(month), int(day))
+            return True
+        except ValueError:
+            return False
 
     def date_weekday(self, date):
-        return ""
+       return date.strftime("%A")
 
     def convert_string_to_date(self, date_str):
-        return ""
+        
+        formats = ["%d/%m/%Y","%d-%m-%Y","%d%m%Y"]
+        obj_date = False
+        
+        for format in formats:
+            try:
+                obj_date = datetime.strptime(date_str, format)
+                break
+            except ValueError:
+                continue
+
+        return obj_date
 
     def get_all_dates(self, month, year):
-        return []
+        n_array = np.arange(
+            start=datetime(int(year), int(month), 1),
+            stop=datetime(int(year), int(month)+1, 1),
+            step=timedelta(days=1),
+            dtype=datetime,
+        )
+        return n_array
 
     def count_days_mounth(self, month, year):
-        return
+        days_of_month = self.get_all_dates(month, year)
+        workdays = np.is_busday(np.array(days_of_month).astype("datetime64[D]"))
+        return np.count_nonzero(workdays)
 
     def get_first_monday(self, year):
-        return ""
+        date = np.busday_offset("%s-05" % year, 0, roll="forward",
+                                holidays=["%s-05-01" % year], weekmask="Mon")
+        return date.tolist().strftime("%d/%m/%Y")
